a6d5cd7de73675dbbfb03b0b5e81d4bc95b7dd53
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-01 18:14:00 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+#include "lib.h"
+bool lettera (char c){
+    if((c>=97 && c<=122)or(c>=65 && c<=90)){
+        return true;
+    }else{
+        return false;
+    }
+}
+char lettera_convertita(char c){
+        if (c>=97 && c<=122){
+            char r= c-32;
+            return r;
+        } else if (c>=65 && c<=90){
+            char r= c+32;
+            return r;
+        }
+    return 'r';
+    }
 
